640b45e92f060d6fae3d1ccaa62b8b82c0700c84
akhilanekkalapudi55 <akhila@ableaura.in>
2021-12-23 11:31:26 +0530

    created a post controller, post model, posts tables and modified api.php

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get all post
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //create
+        return Post::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\ResponsePost
+     */
+    public function show($id)
+    {
+        //show
+        return Post::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //update
+        $post = Post::find($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //delete
+        return Post::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//PostController
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+Route::post('/posts', 'App\Http\Controllers\PostController@store');
+Route::get('/posts/{id}', 'App\Http\Controllers\PostController@show');
+Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update');
+Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy');
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', funtion(){
-    return ['message' => 'hello'];
-})
+
